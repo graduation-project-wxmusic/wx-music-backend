b6455613b143a8ea33206895767465233c2e7ce0
nhm <3059350051@qq.com>
2023-02-26 11:48:41 +0800

    feat 添加路由

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const Koa = require('koa')
+const Router = require('koa-router')
 const app = new Koa()
+const router = new Router()
 
-app.use(async (ctx) => {
-  ctx.body = 'Hello World!'
-})
+const playlist = require('./controller/playlist.js')
+router.use('/playlist', playlist.routes())
+
+app.use(router.routes())
+app.use(router.allowedMethods())
 
 app.listen(3000, () => {
   console.log('The service has started on port 3000.')
---
+++ controller/playlist.js
@@ -0,0 +1,8 @@
+const Router = require('koa-router')
+const router = new Router()
+
+router.get('/list', async (ctx, next) => {
+  // TODO: 查询歌单列表
+})
+
+module.exports = router
